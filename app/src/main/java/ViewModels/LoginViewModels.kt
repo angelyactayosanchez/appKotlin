11cd4bfdffedb1ee3c31a9778a31dbcf9938673f
angel <1523353@utp.edu.pe>
2020-05-18 17:07:43 -0500

    fixed statusbar

---
+++ app/src/main/java/ViewModels/LoginViewModels.kt
@@ -0,0 +1,9 @@
+package ViewModels
+
+import android.app.Activity
+import androidx.lifecycle.ViewModel
+import com.acme.userkotlinowo.databinding.VerifyEmailBinding
+
+class LoginViewModels(activity : Activity, bindingEmail : VerifyEmailBinding) : ViewModel() {
+
+}
--- app/src/main/java/com/acme/userkotlinowo/VerifyEmail.kt
+++ app/src/main/java/com/acme/userkotlinowo/VerifyEmail.kt
@@ -1,16 +1,27 @@
 package com.acme.userkotlinowo
 
+import ViewModels.LoginViewModels
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.acme.userkotlinowo.databinding.VerifyEmailBinding
 
 class VerifyEmail : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.verify_email)
+//        setContentView(R.layout.verify_email)
+
+        var binding = DataBindingUtil.setContentView<VerifyEmailBinding>(this,R.layout.verify_email)
+        binding.emailModel = LoginViewModels(this,binding)
         val window = this.window
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.colorBlue)
+       // window.statusBarColor= this.resources.getColor(R.color.colorBlue,null)
     }
 }
